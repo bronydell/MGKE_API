0b75c8ac95101d46c0054c948013202bd2a085fb
Rostislav <Bronydell@Rostislavs-MacBook-Pro.local>
2016-06-01 13:18:28 +0300

    Added timer. Ready to use

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.al3xable.mgke.TimetableStudent;
 
 public class Main {
 
+
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			TimetableStudent student = new TimetableStudent();
-			student.update();
+		Timer timer = new Timer();
+		TimetableStudent student = new TimetableStudent();
+		timer.schedule( new TimerTask() {
+		    public void run() {
+		       // do your work 
+		    	student.update();
+		    	System.out.println(new Date().toString()+": Update");
+		    }
+		 }, 0, 60*1000*10); //Update each 10 minutes
 	}
-
+	
 }
